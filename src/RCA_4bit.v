9995349f5d4e8a843b06a262b2113b561d13062b
Mahir Ashab Enan <164576423+Mahir2k@users.noreply.github.com>
2026-09-25 00:21:17 -0400

    Implement 4-bit Ripple Carry Adder module

--- src/RCA_4bit.v
+++ src/RCA_4bit.v
@@ -1,1 +1,43 @@
+module RCA_4bit (
+    input [3:0] A,
+    input [3:0] B,
+    input CI,
+    output [3:0] SUM,
+    output CO
+);
 
+wire c1, c2, c3;
+
+FA FA0 (
+    .A(A[0]),
+    .B(B[0]),
+    .CI(CI),
+    .SUM(SUM[0]),
+    .CO(c1)
+);
+
+FA FA1 (
+    .A(A[1]),
+    .B(B[1]),
+    .CI(c1),
+    .SUM(SUM[1]),
+    .CO(c2)
+);
+
+FA FA2 (
+    .A(A[2]),
+    .B(B[2]),
+    .CI(c2),
+    .SUM(SUM[2]),
+    .CO(c3)
+);
+
+FA FA3 (
+    .A(A[3]),
+    .B(B[3]),
+    .CI(c3),
+    .SUM(SUM[3]),
+    .CO(CO)
+);
+
+endmodule
